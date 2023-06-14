769c080fcfa120d6e757c4a83bd5cca955fa059f
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-14 22:48:36 +0600

    Project setup done

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen flex-col gap-5">
+      <h1>Connect Desk</h1>
+      <button className="btn">Started</button>
+    </div>
+  );
+};
+
+export default Home;
