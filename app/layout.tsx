aafef807588f13af7496d98aadef8c18e93e153e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-15 14:29:10 +0600

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
 import { Footer, Header } from "@Components/Shared";
+import "@styles/globals.css";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
