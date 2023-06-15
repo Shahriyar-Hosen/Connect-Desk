88f70e28ff66f9c056e4be5c880e5c68e57a6e42
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-15 10:38:52 +0600

    theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen flex-col gap-5">
-      <h1>Connect Desk</h1>
+      <h1 className="text-accent text-xl font-bold">Connect Desk</h1>
       <button className="btn">Started</button>
     </div>
   );
