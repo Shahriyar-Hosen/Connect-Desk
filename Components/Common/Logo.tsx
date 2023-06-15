06f7e37244ce2ca8ba46d543170eeb477d6f1693
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-15 21:02:33 +0600

    footer initial setup done

--- Components/Common/Logo.tsx
+++ Components/Common/Logo.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 import { ConnectDesk } from "./ConnectDesk";
 
 export const Logo = () => (
-  <Link href="/" className="w-auto">
-    <div className="sm:-mb-[9px] -mb-[9px] mr-[10%] flex justify-end items-end">
-      <Image
-        src={logo}
-        className="w-[22px] h-[22px] sm:w-[30px] sm:h-[30px]"
-        alt=""
-      />
-    </div>
-    <ConnectDesk text="xl" font="Exo" />
-    <p className="text-primary italic sm:text-[70%] text-[53%] font-sans tracking-widest sm:-mt-[5px] -mt-[5px] font-semibold">
-      Business Management Software
-    </p>
-  </Link>
+  <div className="w-fit">
+    <Link href="/">
+      <div className="sm:-mb-[9px] -mb-[9px] mr-[10%] flex justify-end items-end">
+        <Image
+          src={logo}
+          className="w-[22px] h-[22px] sm:w-[30px] sm:h-[30px]"
+          alt=""
+        />
+      </div>
+      <ConnectDesk text="xl" font="Exo" />
+      <p className="text-primary italic sm:text-[70%] text-[53%] font-sans tracking-widest sm:-mt-[5px] -mt-[5px] font-semibold">
+        Business Management Software
+      </p>
+    </Link>
+  </div>
 );
--- Components/Shared/Footer/index.tsx
+++ Components/Shared/Footer/index.tsx
@@ -1,5 +1,73 @@
+import { CommonText, Logo } from "@Components/Common";
+import { contactInfo, info, socialMediaLinks } from "@public/Data";
+import { appStore, playStore } from "@public/assets/images";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => (
-  <div>
-    <h1>This is Footer StaticData</h1>
-  </div>
+  <footer>
+    <div>
+      <div>
+        <div>
+          <Logo />
+
+          <CommonText size="sm">
+            ConnectDesk is a software development company. The company works
+            with business solutions for SME traders.
+          </CommonText>
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <h5 className="font-semibold text-sm">Contact Info</h5>
+          <div className="flex flex-col gap-1">
+            {contactInfo.map(({ icon, number, time, title }, i) => (
+              <div
+                key={i}
+                className="flex justify-start items-center gap-[10px]"
+              >
+                <Image src={icon} alt="" />
+                <CommonText>
+                  {title}: {number} ({time})
+                </CommonText>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>
+        {info.map(({ name, option }, i) => (
+          <div key={i}>
+            <h5 className="font-semibold text-xl mb-6">{name}</h5>
+
+            <div className="flex flex-col justify-center items-start gap-3">
+              {option.map(({ link, title }, i) => (
+                <Link href={link} key={i}>
+                  {title}
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div>
+          <h5 className="font-semibold text-xl mb-6">Download App</h5>
+          <div className="flex justify-start items-center gap-5">
+            <Image src={appStore} alt="" />
+            <Image src={playStore} alt="" />
+          </div>
+        </div>
+        <div>
+          <h5 className="font-semibold text-xl mb-3">Social Media Links</h5>
+
+          <div className="flex justify-start items-center gap-4">
+            {socialMediaLinks.map(({ icon, link, title }) => (
+              <Link href={link} key={title}>
+                <Image src={icon} alt="icon" />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
