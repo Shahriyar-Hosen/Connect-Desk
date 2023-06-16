2a6d60e9c030b30946b30b6658951abe0a0ffd68
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-16 08:40:27 +0600

    Footer organize complete

---
+++ Components/Shared/Footer/CompanySupport.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { infoData } from "@public/Data";
+import { FooterTitle } from ".";
+
+export const CompanySupport = () => (
+  <div className="grid grid-cols-2 mt-0 xl:mt-10 w-auto">
+    {infoData.map(({ name, option }, i) => (
+      <div key={i}>
+        <FooterTitle className="mb-6">{name}</FooterTitle>
+
+        <div className="flex flex-col justify-center items-start gap-3">
+          {option.map(({ link, title }, i) => (
+            <Link href={link} key={i}>
+              {title}
+            </Link>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+);
