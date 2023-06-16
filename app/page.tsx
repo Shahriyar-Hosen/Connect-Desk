1d4d9fc4cca7186871bcab14edf3fcf75fb2adf6
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-16 11:04:09 +0600

    clean upable

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-import {
-  CommonText,
-  ComponentHeader,
-  ConnectDesk,
-  Logo,
-} from "@Components/Common";
-
 const Home = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[85vh] flex-col gap-5">
-      <Logo />
-      <ComponentHeader
-        position="center"
-        title="Check Out All The Important Features Of"
-        subTitle={
-          <span>
-            <ConnectDesk font="sans" /> Software
-          </span>
-        }
-      ></ComponentHeader>
-      <CommonText>
-        Look at the dashboard to gain an understanding of the
-        daily/monthly/yearly business sales, Best Sales Products graph, and
-        Total Income. With a single click, you can access any option.
-      </CommonText>
-    </div>
+    <main>
+      {/* All components */}
+      {/*Components 1 */}
+      {/*Components 2 */}
+    </main>
   );
 };
 
