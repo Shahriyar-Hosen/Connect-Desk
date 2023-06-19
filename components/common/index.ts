7fb476c4dc968fd67cd646133c638c59e78eeb25
Shahriyar Hosen <96829173+Shahriyar-Hosen@users.noreply.github.com>
2023-06-19 12:10:02 +0600

    Delete components/common directory

--- components/common/index.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export * from "./Button";
-export * from "./CommonText";
-export * from "./ComponentHeader";
-export * from "./ConnectDesk";
-export * from "./Container";
-export * from "./Logo";
-export * from "./MainContainer";
