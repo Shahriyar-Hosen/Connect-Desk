8c2414bd5cf253c5eddc3a543b5e2ad975307f21
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-19 17:28:53 +0600

    test

--- Interface/common/IButton.ts
+++ Interface/common/IButton.ts
@@ -2,8 +2,8 @@ import { IChildrenWithClass } from "./IChildren";
 
 export interface IButton extends IChildrenWithClass {
   variant: "primary" | "secondary";
-  rounded?: boolean;
   width?: "full" | "auto";
   text?: "14" | "16" | "18";
   size?: "sm" | "md" | "lg";
+  rounded?: boolean;
 }
