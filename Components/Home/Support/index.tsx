c1eb87be876ed14842950a4d4cc0c6bea345ea6e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-20 20:21:46 +0600

    suport desing updated

--- Components/Home/Support/index.tsx
+++ Components/Home/Support/index.tsx
@@ -15,7 +15,7 @@ import { homeSupportBg, supportLeftImg } from "@public/assets/images";
 
 const Support = () => (
   <BackgroundImage img={homeSupportBg} className="my-2.5 md:my-5 min-h-full">
-    <Container className="flex flex-col md:flex-row justify-center items-center gap-2.5 pt-2.5 pb-10 md:pb-16 ">
+    <Container className="flex flex-col md:flex-row justify-center items-center gap-[5%] pt-2.5 pb-10 md:pb-16 ">
       <div className="w-full md:w-[50%] flex justify-center items-center">
         <Image
           src={supportLeftImg}
