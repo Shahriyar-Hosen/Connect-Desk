9e179a92ec021f730376c684026d3612d3292519
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-20 14:42:12 +0600

    drop down updated

--- Components/Shared/Header/DropdownMenu.tsx
+++ Components/Shared/Header/DropdownMenu.tsx
@@ -6,7 +6,7 @@ import { MenuItem } from "./MenuItem";
 export const DropdownMenu = ({ option, setOpen }: IDropdownMenu) => (
   <ul
     tabIndex={0}
-    className="menu menu-compact dropdown-content -mt-[40px] lg:mt-5 p-2 shadow-2xl shadow-gray-500  bg-white absolute z-[100] rounded-md w-52"
+    className="menu menu-compact dropdown-content -mt-[30px] p-2 shadow-2xl shadow-gray-500  bg-white absolute z-[100] rounded-md w-52"
   >
     {option &&
       option.map(({ label, link }, i) => (
