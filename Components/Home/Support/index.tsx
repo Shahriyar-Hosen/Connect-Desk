55f636f465054ddf806be32bf7d26d4e9eaa8b13
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-20 18:31:00 +0600

    Support almost complete

--- Components/Home/Support/index.tsx
+++ Components/Home/Support/index.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { Container, MainContainer } from "@Components/Common";
+import { BackgroundImage, Container } from "@Components/Common";
+import { homeSupportBg, supportLeftImg } from "@public/assets/images";
+import Image from "next/image";
 
 const Support = () => (
-  <MainContainer>
-    <Container>
-      <h1>Support</h1>
+  <BackgroundImage img={homeSupportBg} className="my-2.5 md:my-5 min-h-full">
+    <Container className="flex flex-col md:flex-row justify-center items-center gap-2.5 pt-2.5 pb-10 md:pb-16 ">
+      <div className="w-full md:w-[50%] flex justify-center items-center">
+        <Image
+          src={supportLeftImg}
+          className="w-[85%] sm:w-[60%] md:w-full"
+          alt=""
+        />
+      </div>
+      <div className="w-full md:w-[50%] flex justify-center items-center md:justify-start">
+        <h1>Support</h1>
+      </div>
     </Container>
-  </MainContainer>
+  </BackgroundImage>
 );
 
 export default Support;
--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -2,20 +2,24 @@ import appStore from "./appStore.svg";
 import arrowHero from "./arrowHero.svg";
 import heroBg from "./heroBg.png";
 import heroLaptop from "./heroLaptop.png";
-import mackbook from "./mackbook.png";
+import homeSupportBg from "./home-support.png";
 import loginLeftImg from "./loginLeftImg.png";
+import mackbook from "./mackbook.png";
 import packageBgLite from "./package-bg-lite.png";
 import packageBg from "./package-bg.png";
 import playStore from "./playStore.svg";
+import supportLeftImg from "./Support-left-img.png";
 
 export {
   appStore,
+  arrowHero,
   heroBg,
   heroLaptop,
-  playStore,
-  mackbook,
-  arrowHero,
+  homeSupportBg,
   loginLeftImg,
-  packageBgLite,
+  mackbook,
   packageBg,
+  packageBgLite,
+  playStore,
+  supportLeftImg,
 };
