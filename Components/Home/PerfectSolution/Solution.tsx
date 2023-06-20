38b591e412cdce73cec6389a56ad644535d2ff7d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-20 22:03:44 +0600

    Perfect Solution

--- Components/Home/PerfectSolution/Solution.tsx
+++ Components/Home/PerfectSolution/Solution.tsx
@@ -4,13 +4,11 @@ import { CommonText } from "@Components/Common";
 import { checkPrimary } from "@public/assets/icon";
 import Image from "next/image";
 
-const Solution = ({ title }: { title: string }) => {
-  return (
-    <div className="w-[136px] flex flex-col justify-center items-center text-center">
-      <Image src={checkPrimary} alt="" />
-      <CommonText>{title}</CommonText>
-    </div>
-  );
-};
+const Solution = ({ title }: { title: string }) => (
+  <div className="w-[136px] flex flex-col justify-center items-center text-center">
+    <Image src={checkPrimary} alt="" />
+    <CommonText>{title}</CommonText>
+  </div>
+);
 
 export default Solution;
