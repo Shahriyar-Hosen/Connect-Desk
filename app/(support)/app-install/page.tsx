e55188ac0d0fe61802cbd29f1ff21483932ab372
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 13:20:01 +0600

    AppInstall top setup done

--- app/(support)/app-install/page.tsx
+++ app/(support)/app-install/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
+import { CommonText, Container, MainContainer } from "@Components/Common";
+import { appInstalImg } from "@public/assets/images";
+import Image from "next/image";
+
 const AppInstall = () => {
   return (
-    <div>
-      <h1>App Install</h1>
-    </div>
+    <MainContainer>
+      <Container className="flex justify-center items-start gap-[15%]">
+        <div className="flex flex-col justify-center items-start">
+          <h1 className="text-4xl font-semibold mb-6">Download App</h1>
+
+          <CommonText className="max-w-[320px]">
+            Now getting our services easily with our mobile app. Download the
+            app and handle your business in your hand
+          </CommonText>
+        </div>
+
+        <Image src={appInstalImg} alt="" />
+      </Container>
+    </MainContainer>
   );
 };
 
