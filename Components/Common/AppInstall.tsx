08384dd5915f173a00ee26c307b708eaf12b1f33
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 15:18:11 +0600

    added link in App Download Img

--- Components/Common/AppInstall.tsx
+++ Components/Common/AppInstall.tsx
@@ -4,10 +4,15 @@ import Image from "next/image";
 
 import { IClassName } from "@Interface";
 import { appStore, playStore } from "@public/assets/images";
+import Link from "next/link";
 
 export const AppDownloadImg = ({ className }: IClassName) => (
   <div className={`flex justify-start items-center w-full ${className}`}>
-    <Image className="w-[50%] h-auto" src={playStore} alt="" />
-    <Image className="w-[50%] h-auto" src={appStore} alt="" />
+    <Link href="/" className="w-[50%]" target="_blank">
+      <Image className="w-full h-auto" src={playStore} alt="" />
+    </Link>
+    <Link href="/" className="w-[50%]" target="_blank">
+      <Image className="w-full h-auto" src={appStore} alt="" />
+    </Link>
   </div>
 );
