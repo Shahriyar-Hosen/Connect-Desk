63543ecfedc98e007c7f6e1c2acf07f93fe96d04
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 17:32:30 +0600

    Packages Page Header added

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { AskedQuestions, PackagesCards } from "@Components/Packages";
+import PageHeader from "@Components/PageHeader";
 
 const Packages = () => (
   <main>
+    <PageHeader
+      pageName="Pricing Plans"
+      mainTitle="Choose The Right Plan For"
+      subTitle={
+        <>
+          Your <span className="text-primary">Business</span>
+        </>
+      }
+    />
     <div className="mt-[60px] pb-[50px]">
       <PackagesCards />
       <AskedQuestions />
