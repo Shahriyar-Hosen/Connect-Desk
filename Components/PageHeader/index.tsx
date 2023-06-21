3d761797838cd006f795def1060671e1bf5f0205
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 17:28:21 +0600

    PageHeader

--- Components/PageHeader/index.tsx
+++ Components/PageHeader/index.tsx
@@ -1,18 +1,14 @@
 import { IPageHeader } from "@Interface";
 
-const PageHeader = ({ mainTitle, pageName, subTitle }: IPageHeader) => {
-  return (
-    <div className="bg-base-100 py-10 rounded-b-[25px] lg:rounded-b-[45px]">
-      <div className="flex flex-col items-center ">
-        <h1 className="text-base font-medium">{pageName}</h1>
-        <div className="w-[94px] h-[2px] bg-primary mb-2"></div>
-        <h1 className="text-[22px] md:text-[32px] font-semibold">
-          {mainTitle}
-        </h1>
-        <h1 className="text-[22px] md:text-[32px] font-semibold">{subTitle}</h1>
-      </div>
+const PageHeader = ({ mainTitle, pageName, subTitle }: IPageHeader) => (
+  <div className="bg-base-100 py-10 rounded-b-[25px] lg:rounded-b-[45px]">
+    <div className="flex flex-col items-center ">
+      <h1 className="text-base font-medium">{pageName}</h1>
+      <div className="w-[94px] h-[2px] bg-primary mb-2"></div>
+      <h1 className="text-[22px] md:text-[32px] font-semibold">{mainTitle}</h1>
+      <h1 className="text-[22px] md:text-[32px] font-semibold">{subTitle}</h1>
     </div>
-  );
-};
+  </div>
+);
 
 export default PageHeader;
