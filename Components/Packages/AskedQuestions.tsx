9cdb8537a224529bb163f22ccf6239f1f42177b9
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 10:14:50 +0600

    Packages page completed

--- Components/Packages/AskedQuestions.tsx
+++ Components/Packages/AskedQuestions.tsx
@@ -3,13 +3,11 @@ import { ComponentHeader, Container, MainContainer } from "@Components/Common";
 import FAQ from "@Components/FAQ";
 import { faqData } from "@public/Data/faq";
 
-export const AskedQuestions = () => {
-  return (
-    <MainContainer>
-      <Container>
-        <ComponentHeader title="Frequently Asked Questions" />
-        <FAQ data={faqData} limit={10} />
-      </Container>
-    </MainContainer>
-  );
-};
+export const AskedQuestions = () => (
+  <MainContainer>
+    <Container>
+      <ComponentHeader title="Frequently Asked Questions" />
+      <FAQ data={faqData} limit={10} />
+    </Container>
+  </MainContainer>
+);
