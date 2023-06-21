2497671446ce2cce3b1fdf2e7009cfb197ddc941
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-21 09:19:59 +0600

    FAQ design complete

---
+++ Components/FAQ/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "@Components/Common";
+
+const FAQ = () => (
+  <Container className="mt-[60px]">
+    <div className="collapse collapse-plus border-t rounded-none border-primary opacity-70 font-[Poppins]">
+      <input type="radio" name="my-accordion-3" checked={true} />
+      <div className="collapse-title text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-black">
+        What is the monthly/yearly charge for this software?
+      </div>
+      <div className="collapse-content">
+        <p>hello</p>
+      </div>
+    </div>
+    <div className="collapse collapse-plus border-t rounded-none border-primary opacity-70 font-[Poppins]">
+      <input type="radio" name="my-accordion-3" />
+      <div className="collapse-title text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-black">
+        Click to open this one and close others
+      </div>
+      <div className="collapse-content">
+        <p>hello</p>
+      </div>
+    </div>
+    <div className="collapse collapse-plus border-t rounded-none border-primary opacity-70 font-[Poppins]">
+      <input type="radio" name="my-accordion-3" />
+      <div className="collapse-title text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-black">
+        Click to open this one and close others
+      </div>
+      <div className="collapse-content">
+        <p>hello</p>
+      </div>
+    </div>
+  </Container>
+);
+
+export default FAQ;
--- Components/Packages/AskedQuestions.tsx
+++ Components/Packages/AskedQuestions.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { ComponentHeader, Container, MainContainer } from "@Components/Common";
+import FAQ from "@Components/FAQ";
 
 export const AskedQuestions = () => {
   return (
     <MainContainer>
       <Container>
         <ComponentHeader title="Frequently Asked Questions" />
+        <FAQ />
       </Container>
     </MainContainer>
   );
