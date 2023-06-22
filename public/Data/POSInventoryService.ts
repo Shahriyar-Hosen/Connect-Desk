904cd94f282788c4a8d031d43e93a285c5c2c007
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-22 17:06:51 +0600

    all data added

--- public/Data/POSInventoryService.ts
+++ public/Data/POSInventoryService.ts
@@ -1,9 +1,81 @@
 import { IPOSInventoryService } from "@Interface";
-import { bill } from "@public/assets/images";
+import {
+  accountingBook,
+  bank,
+  barcode,
+  bill,
+  calculator,
+  data,
+  invoice,
+  invoices,
+  parcel,
+  printer,
+  receipt,
+  report,
+  scanning,
+  sms,
+  videoTutorial,
+} from "@public/assets/images";
 
-export const POSInventoryServices: IPOSInventoryService[] = [
+export const posInventoryServices: IPOSInventoryService[] = [
+  {
+    icon: receipt,
+    title: "Quick Billing Invoice",
+  },
+  {
+    icon: invoices,
+    title: "Product Stock Report",
+  },
+  {
+    icon: invoice,
+    title: "Daily Paid and Due Sales Report",
+  },
+  {
+    icon: accountingBook,
+    title: "Product Purchase Report Date Wise",
+  },
   {
     icon: bill,
-    title: "Quick Billing Invoice",
+    title: "Expense  List Report",
+  },
+  {
+    icon: parcel,
+    title: "Product Return by Customer",
+  },
+  {
+    icon: report,
+    title: "Daily Sales Profit Loss Report",
+  },
+  {
+    icon: sms,
+    title: "Daily Reports Via Mobile SMS",
+  },
+  {
+    icon: bank,
+    title: "Bank Transaction List & Report",
+  },
+  {
+    icon: calculator,
+    title: "Customer and Supplier Ledger",
+  },
+  {
+    icon: printer,
+    title: "Report Printing",
+  },
+  {
+    icon: barcode,
+    title: "Auto Barcode Generator",
+  },
+  {
+    icon: data,
+    title: "Daily Auto & Manual Backup",
+  },
+  {
+    icon: scanning,
+    title: "Barcode Scanner for Easy Trannsaction",
+  },
+  {
+    icon: videoTutorial,
+    title: "Training for User Manual",
   },
 ];
