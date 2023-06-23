f90b44b014bb47bf17a44553ddaddd89ff00cc97
mhrazib121 <mhrazib121@gmail.com>
2023-06-23 11:27:19 +0600

    terms and condition page done

--- Components/Policy/PolicyContainer.tsx
+++ Components/Policy/PolicyContainer.tsx
@@ -1,11 +1,23 @@
 import { CommonText } from "@Components/Common";
 import { IPrivatePolicyData } from "@Interface/data/policyPage";
 
-export const PolicyContainer = ({ data }: { data: IPrivatePolicyData }) => {
+export const PolicyContainer = ({
+  data,
+  textPosition = "center",
+}: {
+  data: IPrivatePolicyData;
+  textPosition?: "center" | "left" | "right";
+}) => {
   const { details, title } = data;
   return (
     <div className="mb-6">
-      <h3 className="text-center text-lg md:text-[26px] font-bold mb-4">
+      <h3
+        className={`${
+          (textPosition === "center" && "text-center") ||
+          (textPosition === "left" && "text-left") ||
+          (textPosition === "right" && "text-right")
+        } text-lg md:text-[24px] font-bold mb-4`}
+      >
         {title}
       </h3>
       {details.map((p) => (
