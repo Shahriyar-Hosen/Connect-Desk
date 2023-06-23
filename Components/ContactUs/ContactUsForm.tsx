0457ec2ab35389cc7395f3128b8d2da08b3ee37e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-23 21:54:41 +0600

    from opacity updated

--- Components/ContactUs/ContactUsForm.tsx
+++ Components/ContactUs/ContactUsForm.tsx
@@ -3,7 +3,7 @@ import { Button, InputField } from "@Components/Common";
 export const ContactUsForm = () => {
   const handleOnChange = () => {};
   return (
-    <div className="bg-base-100 rounded-md p-6 w-full ">
+    <div className="bg-base-100 bg-opacity-50 rounded-md p-6 w-full ">
       <InputField handleOnChange={handleOnChange} placeholder="Your Name" />
       <InputField handleOnChange={handleOnChange} placeholder="Your Address" />
       <InputField handleOnChange={handleOnChange} placeholder="Write subject" />
