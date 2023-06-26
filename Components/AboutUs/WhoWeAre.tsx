7417f8a52aaefbd5d6d547c18443c5407b05f539
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-26 18:44:03 +0600

    update text align

--- Components/AboutUs/WhoWeAre.tsx
+++ Components/AboutUs/WhoWeAre.tsx
@@ -17,12 +17,12 @@ export const WhoWeAre = () => (
 
       <WhoWeAreImg />
 
-      <div className=" grid justify-items-center grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-2.5">
-        <h6 className="col-span-1 w-full text-center text-xl md:text-3xl font-semibold">
+      <div className=" grid justify-items-center grid-cols-1 md:grid-cols-10 max-w-6xl mx-auto gap-2.5">
+        <h6 className="col-span-2 w-full text-center md:text-start text-xl md:text-3xl font-semibold">
           Who We Are
         </h6>
 
-        <p className="col-span-2 w-full text-justify md:text-start text-base md:text-xl text-[#797979]">
+        <p className="col-span-8 w-full text-justify md:text-start text-base md:text-xl text-[#797979]">
           ConnectDesk is a fast growing software manufacturing company in
           Bangladesh providing software and solutions to SMEs, NGO’s, government
           agencies, and private companies. Our team is highly experienced in
