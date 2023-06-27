bf55a59c6c82c4144da3b59702426cd1a85b526d
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-27 08:55:38 +0600

    Client List design initsetup doen

--- app/(support)/client-list/page.tsx
+++ app/(support)/client-list/page.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@Components/Common";
 import PageHeader from "@Components/PageHeader";
+import { download } from "@public/assets/icon";
+import Image from "next/image";
 
 const ClientList = () => (
   <main>
@@ -14,10 +16,15 @@ const ClientList = () => (
       pageName="Client List"
     />
 
-    <div>
+    <div className="w-full max-w-6xl mx-auto flex justify-between items-center">
       <h1 className="text-[26px] font-semibold text-center">Client List</h1>
 
-      <Button text="14" variant="primary">
+      <Button
+        text="14"
+        variant="primary"
+        className="flex justify-center items-center gap-2.5"
+      >
+        <Image src={download} alt="Client List Download icon" />
         Download
       </Button>
     </div>
