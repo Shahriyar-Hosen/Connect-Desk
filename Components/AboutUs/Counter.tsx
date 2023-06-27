b8c04fece1db5a6e2a2ec1e25e67b0a5b99ae42a
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-27 13:52:58 +0600

    added Counter Up Trigger

--- Components/AboutUs/Counter.tsx
+++ Components/AboutUs/Counter.tsx
@@ -1,48 +1,20 @@
 "use client";
 
 import { Container, MainContainer } from "@Components/Common";
-import React, { useState } from "react";
-// import { ScrollTrigger as ReactNode } from "react-scroll-trigger";
-import ScrollTrigger from "react-scroll-trigger";
+import React from "react";
 import { CounterCard } from "./CounterCard";
 
 export const Counter: React.FC = () => {
-  const [isHover, setIsHover] = useState(false);
-  // const Trigger = ScrollTrigger as unknown as ReactNode;
   return (
     <MainContainer>
-      <>
-        <ScrollTrigger
-          onEnter={() => setIsHover(true)}
-          onExit={() => setIsHover(false)}
-          component
-        >
-          <Container>
-            <div className="bg-base-100 bg-opacity-50 py-12 px-8 md:py-20 md:px-16 w-fit lg:w-full max-w-6xl mx-auto rounded-[32px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 justify-items-center">
-              {isHover && (
-                <>
-                  <CounterCard
-                    count={100}
-                    label="M"
-                    title="Client Satisfaction"
-                  />
-                  <CounterCard
-                    count={24}
-                    label=" h"
-                    title="Expert Support Team"
-                  />
-                  <CounterCard count={98} label=" k+" title="Sales Count" />
-                  <CounterCard
-                    count={208}
-                    label=" +"
-                    title="Client Worldwide"
-                  />
-                </>
-              )}
-            </div>
-          </Container>
-        </ScrollTrigger>
-      </>
+      <Container>
+        <div className="bg-base-100 bg-opacity-50 py-12 px-8 md:py-20 md:px-16 w-fit lg:w-full max-w-6xl mx-auto rounded-[32px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 justify-items-center">
+          <CounterCard maxCount={100} label="M" title="Client Satisfaction" />
+          <CounterCard maxCount={24} label=" h" title="Expert Support Team" />
+          <CounterCard maxCount={98} label=" k+" title="Sales Count" />
+          <CounterCard maxCount={208} label=" +" title="Client Worldwide" />
+        </div>
+      </Container>
     </MainContainer>
   );
 };
--- Interface/page/about.ts
+++ Interface/page/about.ts
@@ -1,5 +1,5 @@
 export interface ICounterCard {
-  count: number;
+  maxCount: number;
   label: string;
   title: string;
 }
