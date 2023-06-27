58dd0aa0e63a4482e87f3214c0f7a0c7bd04ce3b
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-27 19:37:19 +0600

    clean up

--- Components/Shared/Footer/index.tsx
+++ Components/Shared/Footer/index.tsx
@@ -1,22 +1,29 @@
 "use client";
 
 import { CommonText, Container } from "@Components/Common";
+import { useIgnorePate } from "@hooks";
 import MainFooterContainer from "./MainFooterContainer";
 
-export const Footer = () => (
-  <footer className="bg-gradient-to-t to-base-200 from-base-100 pt-[60px]">
-    <MainFooterContainer />
+export const Footer = () => {
+  const ignorePath = useIgnorePate();
 
-    <div className="border-t border-primary">
-      <Container className="flex justify-between items-center pt-5 pb-6">
-        <CommonText>
-          Copyright &copy; {new Date().getFullYear()} ConnectDesk
-        </CommonText>
-        <CommonText>All rights reserved</CommonText>
-      </Container>
-    </div>
-  </footer>
-);
+  return (
+    <footer className={`${ignorePath && "hidden"}`}>
+      <section className="bg-gradient-to-t to-base-200 from-base-100 pt-[60px]">
+        <MainFooterContainer />
+
+        <div className="border-t border-primary">
+          <Container className="flex justify-between items-center pt-5 pb-6">
+            <CommonText>
+              Copyright &copy; {new Date().getFullYear()} ConnectDesk
+            </CommonText>
+            <CommonText>All rights reserved</CommonText>
+          </Container>
+        </div>
+      </section>
+    </footer>
+  );
+};
 
 export * from "./CompanySupport";
 export * from "./ContactDetails";
