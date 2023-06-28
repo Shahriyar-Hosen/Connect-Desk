af6ae2cf37a4b3128924892dc95e4cc585df22db
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-28 21:42:46 +0600

    Register update mian container

--- Components/Common/MainContainer.tsx
+++ Components/Common/MainContainer.tsx
@@ -14,7 +14,10 @@ export const MainContainer = ({
     className={`w-full ${!isRemoveMargin && "my-[10px] md:my-[30px]"} ${
       !isRemovePadding && "py-[20px] md:py-[60px]"
     }
-    ${fullScreen && "h-screen w-full flex flex-col justify-center items-center"}
+    ${
+      fullScreen &&
+      "h-full min-h-screen w-full flex flex-col justify-center items-center"
+    }
      ${
        (bg === "default" && "bg-transparent") ||
        (bg === "primary" && "bg-base-100") ||
