7e627af2c23e4dcbacedc95fdefaf4a328ae2dd1
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-06-28 21:56:11 +0600

    input field update

--- Components/Common/InputField.tsx
+++ Components/Common/InputField.tsx
@@ -4,22 +4,20 @@ import { InputFieldProps } from "@Interface";
 
 export const InputField = ({
   label,
-  type = "text",
   placeholder,
+  type = "text",
   width = "full",
   handleOnChange,
-}: InputFieldProps) => {
-  return (
-    <div className="w-full my-2">
-      <p className="text-base font-medium text-neutral mb-3">{label}</p>
-      <input
-        className={`border border-primary border-opacity-20 rounded-md p-1.5 md:p-2 lg:p-3 text-sm ${
-          (width === "full" && "w-full") || (width === "half" && "w-1/2")
-        }`}
-        onChange={(e) => handleOnChange(e)}
-        type={type}
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
+}: InputFieldProps) => (
+  <div className="w-full my-2">
+    <p className="text-base font-medium text-neutral mb-3">{label}</p>
+    <input
+      className={`border border-primary border-opacity-20 rounded-md p-1.5 md:p-2 lg:p-3 text-sm focus:outline-primary ${
+        (width === "full" && "w-full") || (width === "half" && "w-1/2")
+      }`}
+      onChange={(e) => handleOnChange(e)}
+      type={type}
+      placeholder={placeholder}
+    />
+  </div>
+);
