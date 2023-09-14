4e99b4f5efab68c40c8740f324ccbf15cc0d5e19
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-14 23:42:32 +0600

    This is Testing Purpose deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { Logo } from "@/Components/common";
+import { fullLogo } from "@/public/assets";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center mt-10">
-      <Logo />
+    <div className="flex flex-col justify-center items-center mt-10">
+      <Image src={fullLogo} alt="" />
+      <p className="text-2xl font-extrabold mt-5 text-primary">
+        This is Testing Purpose deploy
+      </p>
     </div>
   );
 };
