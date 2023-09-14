8878ffa4372999eca57517df25dab877da1a2c03
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-14 23:28:55 +0600

    add logo

---
+++ Components/common/index.ts
@@ -0,0 +1,2 @@
+export * from "./ConnectDesk";
+export * from "./Logo";
---
+++ public/assets/index.ts
@@ -0,0 +1,5 @@
+import fullLogo from "./logo.png";
+import logo from "./main-logo.png";
+import takaLogo from "./taka-logo.png";
+
+export { fullLogo, logo, takaLogo };
