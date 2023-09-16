1d683fdfbbb1d3d24e0d554910fb8dd45a9212c3
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 20:10:52 +0600

    connect desk test

--- Components/common/ConnectDesk.tsx
+++ Components/common/ConnectDesk.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 import { twMerge } from "tailwind-merge";
 
 export const ConnectDesk: FC<IClassName> = ({ className }) => (
-  <span className={twMerge("text-xl text-secondary font-semibold", className)}>
+  <p className={twMerge("text-xl text-secondary font-semibold", className)}>
     Connect<span className="text-primary">Desk</span>
-  </span>
+  </p>
 );
