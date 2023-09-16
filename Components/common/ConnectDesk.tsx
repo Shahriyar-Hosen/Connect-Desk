e3daddfdabe2e0444dc2144ef6074085a4f686f6
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 20:20:05 +0600

    Connect Desk clean up

--- Components/common/ConnectDesk.tsx
+++ Components/common/ConnectDesk.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { IClassName } from "@/Interface";
 import { FC } from "react";
 import { twMerge } from "tailwind-merge";
 
+import { IClassName } from "@/Interface";
+
 export const ConnectDesk: FC<IClassName> = ({ className }) => (
-  <p className={twMerge("text-xl text-secondary font-semibold", className)}>
+  <p
+    className={twMerge(
+      "text-xl text-secondary font-semibold font-poppins",
+      className
+    )}
+  >
     Connect<span className="text-primary">Desk</span>
   </p>
 );
