695dd60616f0f8bb55365d872150edcc14439389
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 20:07:52 +0600

    test

--- Components/common/ConnectDesk.tsx
+++ Components/common/ConnectDesk.tsx
@@ -1,26 +1,11 @@
 "use client";
 
-import { ITextAndFont } from "@/Interface";
+import { IClassName } from "@/Interface";
 import { FC } from "react";
+import { twMerge } from "tailwind-merge";
 
-export const ConnectDesk: FC<ITextAndFont> = ({
-  size = "md",
-  font = "poppins",
-}) => (
-  <span
-    className={`${
-      (size === "md" && "text-[20px]") ||
-      (size === "lg" && `text-[24px] lg:text-[28px]`) ||
-      (size === "xl" && "text-[26px] md:text-[28px] lg:text-[32px]")
-    } 
-      ${
-        (font === "poppins" && "font-poppins") ||
-        (font === "exo" && "font-exo") ||
-        (font === "sans" && "font-sans")
-      }
-      
-    text-secondary font-semibold`}
-  >
+export const ConnectDesk: FC<IClassName> = ({ className }) => (
+  <span className={twMerge("text-xl text-secondary font-semibold", className)}>
     Connect<span className="text-primary">Desk</span>
   </span>
 );
