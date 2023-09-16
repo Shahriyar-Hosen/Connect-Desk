13db9cda29504a01ac1fca793770a62e7a337a2f
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 19:28:08 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 
+import { ConnectDesk } from "@/Components/common";
 import { fullLogo, logoSvg } from "@/public/assets";
 import Image from "next/image";
 
 const Home = () => (
   <div className="flex flex-col justify-center items-center mt-10">
     <Image src={fullLogo} alt="" />
-    <p className="text-2xl font-extrabold mt-5 text-primary font-poppins">
+    <p className="text-2xl font-extrabold mt-5 text-primary font-poppins mb-5">
       This is Testing Purpose deploy (poppins)
-    </p>
-    <p className="text-2xl font-extrabold mt-5 text-primary font-exo mb-10">
-      This is Testing Purpose deploy (exo)
     </p>
 
     <Image src={logoSvg} alt="" />
+
+    <p className="text-2xl font-extrabold mt-5 text-primary font-exo mb-5">
+      This is Testing Purpose deploy (exo)
+    </p>
+
+    <ConnectDesk />
   </div>
 );
 
