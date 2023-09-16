4be778949ecdadcbb5456662a946d5960556ce63
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 18:51:58 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,13 @@
 import { fullLogo } from "@/public/assets";
 import Image from "next/image";
 
-const Home = () => {
-  return (
-    <div className="flex flex-col justify-center items-center mt-10">
-      <Image src={fullLogo} alt="" />
-      <p className="text-2xl font-extrabold mt-5 text-primary">
-        This is Testing Purpose deploy
-      </p>
-    </div>
-  );
-};
+const Home = () => (
+  <div className="flex flex-col justify-center items-center mt-10">
+    <Image src={fullLogo} alt="" />
+    <p className="text-2xl font-extrabold mt-5 text-primary">
+      This is Testing Purpose deploy
+    </p>
+  </div>
+);
 
 export default Home;
