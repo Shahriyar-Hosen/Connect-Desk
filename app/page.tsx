f2fe38de93adf78518d5485662740b472f2a3df5
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-09-16 09:15:01 +0600

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import { logoSvg } from "@/public/assets";
 import Image from "next/image";
 import { FC } from "react";
 
-const Home: FC = () => {
-  return (
-    <main className="flex flex-col justify-center items-center gap-5 text-center mt-5">
-      <Image src={logoSvg} alt="" />
-      <h1 className="font-Exo text-primary text-2xl font-bold">
-        This is <span className="text-secondary">Home</span> Page
-      </h1>
-    </main>
-  );
-};
+const Home: FC = () => (
+  <main className="flex flex-col justify-center items-center gap-5 text-center mt-5">
+    <Image src={logoSvg} alt="" />
+    <h1 className="font-Exo text-primary text-2xl font-bold">
+      This is <span className="text-secondary">Home</span> Page
+    </h1>
+  </main>
+);
 
 export default Home;
