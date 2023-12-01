988f4e5781a756fe1ffe0fb6f21d8937bef38f4c
forks007 <farhancuet014@gmail.com>
2023-12-01 20:37:39 +0600

    message from founder

--- app/[lng]/(public)/(more)/message-from-founder/page.tsx
+++ app/[lng]/(public)/(more)/message-from-founder/page.tsx
@@ -5,66 +5,91 @@ import Image from "next/image";
 import { CommonText, Container, MainContainer, PageHeader } from "@/components";
 
 import { founder } from "@/public/assets/images";
+import { Trans } from "react-i18next";
+import { IIntlParams } from "@/Interface";
+import { FC } from "react";
+import { useTranslation } from "@/app/i18n/client";
 
-const MessageFromFounder = () => (
-  <main>
-    <PageHeader
-      pageName="Message From Founder"
-      mainTitle={
-        <>
-          Message From <span className="text-primary">Founder</span>
-        </>
-      }
-    />
+const MessageFromFounder: FC<IIntlParams> = ({ params: { lng } }) => {
+  const { t } = useTranslation(lng, ["more"]);
+  return (
+    <main>
+      <PageHeader
+        pageName={t("messageFromFounder.pageName")}
+        mainTitle={
+          <>
+            <Trans
+              i18nKey="messageFromFounder.mainTitle"
+              components={{ spanTag: <span className="text-primary" /> }}
+              t={t}
+              ns={"more"}
+            />
+          </>
+        }
+      />
 
-    <MainContainer isRemoveMargin>
-      <Container>
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center md:justify-start items-center  md:items-start gap-10 md:gap-20 py-5 md:py-0">
-          <div className="w-full max-w-[342px] flex flex-col gap-3">
-            <div className="border border-primary p-3 w-full rounded-[10px]">
-              <Image src={founder} className="w-full rounded-[10px]" alt="" />
+      <MainContainer isRemoveMargin>
+        <Container>
+          <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center md:justify-start items-center  md:items-start gap-10 md:gap-20 py-5 md:py-0">
+            <div className="w-full max-w-[342px] flex flex-col gap-3">
+              <div className="border border-primary p-3 w-full rounded-[10px]">
+                <Image src={founder} className="w-full rounded-[10px]" alt="" />
+              </div>
+
+              <div className="w-full bg-primary font-semibold rounded-[10px] px-[15px] py-3 text-white">
+                <span className="text-2xl">MD. Hanif Uddin</span>
+                <span className="text-xl">Founder & CEO, ConnectDesk</span>
+              </div>
             </div>
 
-            <div className="w-full bg-primary font-semibold rounded-[10px] px-[15px] py-3 text-white">
-              <span className="text-2xl">MD. Hanif Uddin</span>
-              <span className="text-xl">Founder & CEO, ConnectDesk</span>
+            <div className="flex flex-col justify-center items-center md:items-start gap-5 md:gap-10">
+              <div className="w-fit bg-base-100 bg-opacity-50 px-[30px] py-[13px] rounded-[10px] boxShadow">
+                <span className="text-primary text-2xl font-semibold">
+                  {t("messageFromFounder.messageFromFounder")}
+                </span>
+              </div>
+
+              <div className="w-fit bg-base-100 bg-opacity-50 px-[30px] py-[13px] rounded-[10px] boxShadow flex flex-col justify-center items-start gap-5 max-w-[610px]">
+                <CommonText>
+                  <Trans
+                    i18nKey="messageFromFounder.messageParaOne"
+                    components={{
+                      strongTag: <strong />,
+                    }}
+                    t={t}
+                    ns={"more"}
+                    values={{ cd: "ConnectDesk" }}
+                  />
+                </CommonText>
+                <CommonText>
+                  <Trans
+                    i18nKey="messageFromFounder.messageParaTwo"
+                    components={{
+                      strongTag: <strong />,
+                    }}
+                    t={t}
+                    ns={"more"}
+                    values={{ cd: "ConnectDesk" }}
+                  />
+                </CommonText>
+                <CommonText>
+                  <Trans
+                    i18nKey="messageFromFounder.messageParaThree"
+                    components={{
+                      strongTag: <strong />,
+                    }}
+                    t={t}
+                    ns={"more"}
+                    values={{ cd: "ConnectDesk" }}
+                  />
+                </CommonText>
+              </div>
             </div>
           </div>
-
-          <div className="flex flex-col justify-center items-center md:items-start gap-5 md:gap-10">
-            <div className="w-fit bg-base-100 bg-opacity-50 px-[30px] py-[13px] rounded-[10px] boxShadow">
-              <span className="text-primary text-2xl font-semibold">
-                Message From Founder
-              </span>
-            </div>
-
-            <div className="w-fit bg-base-100 bg-opacity-50 px-[30px] py-[13px] rounded-[10px] boxShadow flex flex-col justify-center items-start gap-5 max-w-[610px]">
-              <CommonText>
-                I am delighted to welcome you to <strong>ConnectDesk</strong>,
-                and I appreciate your interest in our company. At{" "}
-                <strong>ConnectDesk</strong>, our mission is to provide
-                innovative solutions that exceed our customers’ expectations
-                while delivering outstanding customer service.
-              </CommonText>
-              <CommonText>
-                We take pride in our comprehensive range of services, including
-                software development, web design, e-commerce solutions, digital
-                marketing, and more. Our team of skilled and experienced
-                professionals is committed to delivering high-quality products
-                and services that are tailored to meet your specific needs.
-              </CommonText>
-              <CommonText>
-                Thank you for considering <strong>ConnectDesk</strong> for your
-                technology needs. Please don’t hesitate to contact us if you
-                have any questions or if you would like to discuss how we can
-                assist you.
-              </CommonText>
-            </div>
-          </div>
-        </div>
-      </Container>
-    </MainContainer>
-  </main>
-);
+        </Container>
+      </MainContainer>
+    </main>
+  );
+};
 
 export default MessageFromFounder;
