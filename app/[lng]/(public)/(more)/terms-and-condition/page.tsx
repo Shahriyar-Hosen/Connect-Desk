c853946225190a27d61f6374784cd2c49321b65b
forks007 <farhancuet014@gmail.com>
2023-12-01 20:56:27 +0600

    wip

--- app/[lng]/(public)/(more)/terms-and-condition/page.tsx
+++ app/[lng]/(public)/(more)/terms-and-condition/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { IIntlParams } from "@/Interface";
+import { useTranslation } from "@/app/i18n/client";
 import {
   ComponentHeader,
   MainContainer,
@@ -8,25 +10,33 @@ import {
 } from "@/components";
 import { termsAndConditionData } from "@/public/Data";
 import { FC } from "react";
+import { Trans } from "react-i18next";
 
-const PrivacyPolicy: FC = () => (
-  <main>
-    <PageHeader
-      pageName="Terms And Conditions"
-      mainTitle={
-        <span>
-          Terms And <span className="text-primary">Conditions</span>
-        </span>
-      }
-    />
-    <div className="w-[80%] md:w-[90%] lg:w-[70%] mx-auto ">
-      <MainContainer isRemoveMargin>
-        <ComponentHeader title="সফটওয়্যার ব্যবহারের শর্তাবলী" />
-      </MainContainer>
-      {termsAndConditionData.map((terms) => (
-        <PolicyContainer textPosition="left" {...terms} key={terms.title} />
-      ))}
-    </div>
-  </main>
-);
-export default PrivacyPolicy;
+const TermsAndCondition: FC<IIntlParams> = ({ params: { lng } }) => {
+  const { t } = useTranslation(lng, ["more"]);
+  return (
+    <main>
+      <PageHeader
+        pageName={t("termsAndCondition.pageName")}
+        mainTitle={
+          <span>
+            <Trans
+              i18nKey="termsAndCondition.mainTitle"
+              components={{ spanTag: <span className="text-primary" /> }}
+              t={t}
+            />
+          </span>
+        }
+      />
+      <div className="w-[80%] md:w-[90%] lg:w-[70%] mx-auto ">
+        <MainContainer isRemoveMargin>
+          <ComponentHeader title="সফটওয়্যার ব্যবহারের শর্তাবলী" />
+        </MainContainer>
+        {termsAndConditionData.map((terms) => (
+          <PolicyContainer textPosition="left" {...terms} key={terms.title} />
+        ))}
+      </div>
+    </main>
+  );
+};
+export default TermsAndCondition;
