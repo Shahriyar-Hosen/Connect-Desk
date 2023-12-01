067672955c2c80ee13d2889c11e9b055dfb0ab8a
forks007 <farhancuet014@gmail.com>
2023-12-01 22:55:14 +0600

    changes local swithcer

--- components/Shared/Header/LanguageSwitcher.tsx
+++ components/Shared/Header/LanguageSwitcher.tsx
@@ -1,9 +1,25 @@
+"use client";
 import { IIntlLang, IIntlTranslator } from "@/Interface";
 import { languages } from "@/app/i18n/settings";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 export const LanguageSwitcher: FC<IIntlLang> = ({ lng }) => {
+  const { i18n } = useTranslation();
+  const currentLocale = i18n.language;
+  const router = useRouter();
+  const currentPathname = usePathname();
+
+  const handleClick = (lng: string) => {
+    const newLocale = lng;
+
+    router.push(currentPathname.replace(`/${currentLocale}`, `/${newLocale}`));
+
+    router.refresh();
+  };
+
   return (
     <>
       {languages
@@ -13,6 +29,7 @@ export const LanguageSwitcher: FC<IIntlLang> = ({ lng }) => {
             <span
               key={l}
               className="rounded-full bg-primary p-2 text-white w-10 h-10"
+              onClick={() => handleClick(l)}
             >
               {index > 0 && "or"}
               <Link href={`/${l}`}>{l}</Link>
