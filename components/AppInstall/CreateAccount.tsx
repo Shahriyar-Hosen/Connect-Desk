e7c0c05ec5a660fcc12b61ce72b2c6384890bdf7
forks007 <farhancuet014@gmail.com>
2023-12-12 00:15:10 +0600

    app install done

--- components/AppInstall/CreateAccount.tsx
+++ components/AppInstall/CreateAccount.tsx
@@ -3,14 +3,17 @@
 import { createAccount } from "@/public/Data";
 import { FC } from "react";
 import { CommonText, ListItem } from "..";
+import { IIntlTranslator } from "@/Interface";
 
-export const CreateAccount: FC = () => (
+export const CreateAccount: FC<IIntlTranslator> = ({ t }) => (
   <div className="max-w-[431px]">
-    <CommonText className="font-medium">How to create an account:</CommonText>
+    <CommonText className="font-medium">
+      {t("HowToCreateAccount", { ns: "app-install" })}
+    </CommonText>
 
     <div className="mt-3 md:mt-4 flex flex-col justify-center items-start gap-1">
       {createAccount.map((item) => (
-        <ListItem label={item} key={item} />
+        <ListItem label={t(item, { ns: "app-install" })} key={item} />
       ))}
     </div>
   </div>
