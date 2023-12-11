f2aaa453bbf2acd6972f336f8fe0557ef9850b06
forks007 <farhancuet014@gmail.com>
2023-12-11 23:53:39 +0600

    software setup done

--- app/[lng]/(public)/(support)/software-setup/page.tsx
+++ app/[lng]/(public)/(support)/software-setup/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { IIntlParams } from "@/Interface";
+import { useTranslation } from "@/app/i18n/client";
 import {
   CommonText,
   Container,
@@ -7,32 +9,45 @@ import {
   PageHeader,
   Video,
 } from "@/components";
+import { FC } from "react";
+import { Trans } from "react-i18next";
 
-const SoftwareSetup = () => (
-  <main>
-    <PageHeader
-      pageName="Software Setup"
-      mainTitle={
-        <>
-          Software <span className="text-primary">Setup</span>
-        </>
-      }
-    />
+const SoftwareSetup: FC<IIntlParams> = ({ params: { lng } }) => {
+  const { t } = useTranslation(lng, ["common", "software-setup"]);
+  return (
+    <main>
+      <PageHeader
+        pageName={t("pageName", { ns: "software-setup" })}
+        mainTitle={
+          <>
+            {" "}
+            <Trans
+              i18nKey="mainTitle"
+              components={{ spanTag: <span className="text-primary" /> }}
+              t={t}
+              ns={"software-setup"}
+            />
+          </>
+        }
+      />
 
-    <MainContainer>
-      <Container>
-        <CommonText className="font-semibold text-center md:-mt-10" text="4xl">
-          ফ্রী সাইনআপ বা অ্যাকাউন্ট তৈরি করার জন্য নিচের ভিডিও দেখুন
-        </CommonText>
+      <MainContainer>
+        <Container>
+          <CommonText
+            className="font-semibold text-center md:-mt-10"
+            text="4xl"
+          >
+            {t("introPara", { ns: "software-setup" })}
+          </CommonText>
 
-        <Video
-          size="lg"
-          className="mt-[50px]"
-          url="https://www.youtube.com/embed/o2W8_mvLuxU"
-        />
-      </Container>
-    </MainContainer>
-  </main>
-);
-
+          <Video
+            size="lg"
+            className="mt-[50px]"
+            url="https://www.youtube.com/embed/o2W8_mvLuxU"
+          />
+        </Container>
+      </MainContainer>
+    </main>
+  );
+};
 export default SoftwareSetup;
