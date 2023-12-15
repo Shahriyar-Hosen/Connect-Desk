d41d9d2da8b1ea325f147b80068af32bf30e2a94
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-12-15 18:59:24 +0600

    solve error

--- components/ContactUs/ContactUsContainer.tsx
+++ components/ContactUs/ContactUsContainer.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { IIntlLang, IIntlTranslator } from "@/Interface";
+import { IIntlTranslator } from "@/Interface";
 import { FC } from "react";
 import { ContactDetails, ContactUsForm, MainContainer } from "..";
 
-export const ContactUsContainer: FC<IIntlTranslator & IIntlLang> = ({
-  t,
-  lng,
-}) => (
+export const ContactUsContainer: FC<IIntlTranslator> = ({ t }) => (
   <MainContainer>
     <div className="flex flex-col max-w-[1020px] lg:flex-row gap-6 w-[90%] lg:w-[75%] mx-auto">
       <ContactDetails t={t} />
