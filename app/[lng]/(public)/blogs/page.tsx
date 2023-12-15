fd7b9d5f4be02a39b72658bd8aa4d006b7b34632
forks007 <farhancuet014@gmail.com>
2023-12-15 22:43:34 +0600

    blogs done

--- app/[lng]/(public)/blogs/page.tsx
+++ app/[lng]/(public)/blogs/page.tsx
@@ -15,7 +15,7 @@ const BlogsPage: FC<IIntlParams> = async ({ params: { lng } }) => {
       />
       <div className="w-[80%] md:w-[90%] lg:w-[70%] max-w-6xl mx-auto">
         {blogsData.map((blog) => (
-          <Blog key={blog.id} {...blog} />
+          <Blog key={blog.id} {...blog} lng={lng} />
         ))}
       </div>
     </main>
