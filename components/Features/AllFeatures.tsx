1108c4894041758460888910f360a5593ddc575e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-12-17 16:51:41 +0600

    Feature

--- components/Features/AllFeatures.tsx
+++ components/Features/AllFeatures.tsx
@@ -1,18 +1,18 @@
 "use client";
 
+import { IIntlLang } from "@/Interface";
+import { useTranslation } from "@/app/i18n/client";
 import { features } from "@/public/Data";
 import { FC } from "react";
 import { MainContainer } from "..";
 import { Feature } from "./Feature";
-import { IIntlLang } from "@/Interface";
-import { useTranslation } from "@/app/i18n/client";
 
 export const AllFeatures: FC<IIntlLang> = ({ lng }) => {
   const { t } = useTranslation(lng, "features");
   return (
     <MainContainer isRemoveMargin>
       {features.map((feature) => (
-        <Feature key={feature.id} {...feature} t={t} />
+        <Feature lng={lng} key={feature.id} {...feature} t={t} />
       ))}
     </MainContainer>
   );
--- components/Features/Feature.tsx
+++ components/Features/Feature.tsx
@@ -1,42 +1,47 @@
 "use client";
 
-import { IFeaturesPage, IIntlTranslator } from "@/Interface";
+import { IFeaturesPage, IIntlLang, IIntlTranslator } from "@/Interface";
 import Image from "next/image";
 import { FC } from "react";
 import { CommonText, ComponentHeader } from "..";
 
-export const Feature: FC<IFeaturesPage & IIntlTranslator> = ({
+export const Feature: FC<IFeaturesPage & IIntlTranslator & IIntlLang> = ({
   id,
   image,
   paragraph,
   title,
   t,
+  lng,
 }) => (
   <div
     className={`flex flex-col ${
       id % 2 === 1 ? "md:flex-row" : "md:flex-row-reverse"
     } md:gap-14 lg:gap-24 justify-center md:mb-20 px-10`}
   >
-    <div className="">
+    <div>
       <Image
-        className="w-full max-w-[530px] mx-auto"
         src={image}
         alt={title.second}
+        className="w-full max-w-[530px] mx-auto"
       />
     </div>
-    <div className="max-w-[342px] flex flex-col justify-center mx-auto md:mx-0 mt-8 md:mt-0">
+    <div
+      className={`flex flex-col justify-center mx-auto md:mx-0 mt-8 md:mt-0 ${
+        lng === "bn" ? "max-w-[350px]" : "max-w-[342px]"
+      }`}
+    >
       <ComponentHeader
         position="start"
         title={
-          <span>
+          <>
             {t(title.first)}{" "}
-            <span className="text-primary">{t(title.second)}</span>
-          </span>
+            <span className="text-primary">{t(title.second)}</span>{" "}
+            <span className="text-secondary">{t(title.third)}</span>
+          </>
         }
-        subTitle={<span className="text-secondary">{t(title.third)}</span>}
       />
       <br />
-      <CommonText>{t(paragraph)}</CommonText>
+      <CommonText className="max-w-[350px]">{t(paragraph)}</CommonText>
     </div>
   </div>
 );
