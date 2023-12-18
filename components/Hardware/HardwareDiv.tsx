252266133d367af49cba948ede5f1f72dca02a14
forks007 <farhancuet014@gmail.com>
2023-12-18 15:55:49 +0600

    hardware correction

--- components/Hardware/HardwareDiv.tsx
+++ components/Hardware/HardwareDiv.tsx
@@ -14,9 +14,7 @@ export const HardwareDiv: FC<IProps> = ({ lng, title }) => {
   const { t } = useTranslation(lng, "hardware");
   return (
     <div className="my-16">
-      <ComponentHeader
-        title={<span>{t("PrivacyPolicy", { ns: "privacy-policy" })}</span>}
-      />
+      <ComponentHeader title={<span>{t(title, { ns: "hardware" })}</span>} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-[90%] lg:max-w-[1020px] mx-auto mt-[30px] md:mt-[70px]">
         {hardwareData.map((hardware, i) => (
           <div key={i} className={`${hardware.category !== title && "hidden"}`}>
