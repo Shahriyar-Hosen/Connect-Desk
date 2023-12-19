af61d16f49cc7d0afd947bb75439ec8d01a8d1cd
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-12-19 15:58:38 +0600

    clean up

--- app/(without header footer)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { IChildren } from "@/Interface";
-import { FC } from "react";
-
-const WithoutHeaderFooterLayout: FC<IChildren> = ({ children }) => (
-  <main>{children}</main>
-);
-
-export default WithoutHeaderFooterLayout;
