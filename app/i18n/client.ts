b8117d1745b98f413519fc6e678097b660fe280e
Shahriyar Hosen <96829173+Shahriyar-Hosen@users.noreply.github.com>
2024-09-30 06:53:10 +0600

    React Hook "useState" is called conditionally. React Hooks must be called in the exact same order in every component render.

--- app/i18n/client.ts
+++ app/i18n/client.ts
@@ -41,27 +41,28 @@ export function useTranslation(
   const ret = useTranslationOrg(ns, options);
   const { i18n } = ret;
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (activeLng === i18n.resolvedLanguage) return;
+    setActiveLng(i18n.resolvedLanguage);
+  }, [activeLng, i18n.resolvedLanguage]);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (!lng || i18n.resolvedLanguage === lng) return;
+    i18n.changeLanguage(lng);
+  }, [lng, i18n]);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    if (cookies.i18next === lng) return;
+    setCookies(cookieName, lng, { path: "/" });
+  }, [lng, cookies.i18next, setCookies]);
+
   if (runOnServerSide && lng && i18n.resolvedLanguage !== lng) {
     i18n.changeLanguage(lng);
-  } else {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      if (activeLng === i18n.resolvedLanguage) return;
-      setActiveLng(i18n.resolvedLanguage);
-    }, [activeLng, i18n.resolvedLanguage]);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      if (!lng || i18n.resolvedLanguage === lng) return;
-      i18n.changeLanguage(lng);
-    }, [lng, i18n]);
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-      if (cookies.i18next === lng) return;
-      setCookies(cookieName, lng, { path: "/" });
-    }, [lng, cookies.i18next, setCookies]);
   }
+  
   return ret;
 }
